f06f9cfc3020ae6b0edf05bd39473338affa655c
SebasDeveloper <jimenezrodiriguezsebastian@gmail.com>
2026-07-07 12:30:33 -0600

    changes really inrevelands

--- setup/windows.bat
+++ setup/windows.bat
@@ -4,8 +4,8 @@ cd ..
 @echo on
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
-haxelib install lime 8.1.2
-haxelib install openfl 9.3.3
+haxelib install lime 8.3.2
+haxelib install openfl 9.5.2
 haxelib install flixel 6.1.2
 haxelib install flixel-addons 4.0.1
 haxelib install flixel-tools 1.5.1
@@ -13,9 +13,9 @@ haxelib install hscript-iris 1.1.3
 haxelib install tjson 1.4.0
 haxelib install hxdiscord_rpc 1.3.0
 haxelib install hxvlc 2.3.0 --skip-dependencies
-haxelib set lime 8.1.2
-haxelib set openfl 9.3.3
-haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
+haxelib set lime 8.3.2
+haxelib set openfl 9.5.2
+haxelib install flxanimate 4.0.0 
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
